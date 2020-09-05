66308d3f26d550169d9c6a29f6733b29d356b3d3
ABIGAIL VANESSA SEPULVEDA <a.sepulveda@alumnos.duoc.cl>
2020-09-04 23:42:24 -0400

    Proyecto base

---
+++ gestor-tareas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gestor_tareas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
