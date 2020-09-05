6efd2b631f5912409ecb298ed293ea51dc42fecb
Felipe Segura Cerda <l.segurac@alumnos.duoc.cl>
2020-09-04 21:24:02 -0400

    Add project files.

---
+++ gestor-tareas/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using gestor_tareas.Models;
+using System.Web.Mvc;
+
+
+namespace gestor_tareas.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Verify(Usuario user ) {
+            View();
+        }
+    }
+}
---
+++ gestor-tareas/Models/usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+namespace gestor_tareas.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string rol_id { get; set; }
+        public string unit_id { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+}
